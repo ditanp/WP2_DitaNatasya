40e0cea00dbebcd6cd04114df83d09b3c29266dd
ditanp <natasyadita15@gmail.com>
2021-09-29 22:26:49 +0700

    upload framework ci3

--- application/views/view-data-matakuliah.php
+++ application/views/view-data-matakuliah.php
@@ -33,7 +33,7 @@
                 </tr>
                 <tr>
                     <td colspan="3" align="center">
-                        <a href="<?= base_url('matakuliah');?>">Kembali</a>
+                        <!-- <a href="<?= base_url('matakuliah');?>">Kembali</a> -->
                     </td>
                 </tr>
             </table>
