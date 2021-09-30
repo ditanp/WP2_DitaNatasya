1f26d692c531a78b9c64c7d83909ca5fc6e8d334
ditanp <natasyadita15@gmail.com>
2021-09-30 22:03:06 +0700

    revisi file

--- application/views/view-data-matakuliah.php
+++ application/views/view-data-matakuliah.php
@@ -33,7 +33,7 @@
                 </tr>
                 <tr>
                     <td colspan="3" align="center">
-                        <!-- <a href="<?= base_url('matakuliah');?>">Kembali</a> -->
+                        <?= base_url('matakuliah');?>">Kembali</a>
                     </td>
                 </tr>
             </table>
